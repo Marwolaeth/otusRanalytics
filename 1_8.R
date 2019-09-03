dc9561d1ad9f6a26f18ad3c48998741de45a3be6
Marwolaeth <a.pawluczenko@gmail.com>
2019-09-03 18:10:28 +0000

    Lesson 1-8

---
+++ 1_8.R
@@ -0,0 +1,11 @@
+if(!require('pacman')) install.packages('pacman')
+pacman::p_load(tidyverse)
+
+(x <- c('X1', 'X2', 'Y1', 'Y2'))
+str_replace(x, 'Y', 'Z')
+str_replace_all(x, 'Y', 'Z')
+
+?p_load
+
+# отлично!
+pacman::p_load(tidytext, tm, udpipe, update = TRUE)
