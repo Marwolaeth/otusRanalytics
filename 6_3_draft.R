940e4f1910db1b3c69f54ea7038a84f7dd29c780
Marwolaeth <a.pawluczenko@gmail.com>
2019-12-25 17:01:25 +0000

    6_3 more

--- 6_3_draft.R
+++ 6_3_draft.R
@@ -1,16 +1,30 @@
 N <- 200 # Для обеих групп
 p <- .3  # Для обеих групп
 set.seed(191222)
-A <- sample(c(FALSE, TRUE), size = N, replace = TRUE, prob = c(1-p, p))
-B <- sample(c(FALSE, TRUE), size = N, replace = TRUE, prob = c(1-p, p))
+# A <- sample(c(FALSE, TRUE), size = N, replace = TRUE, prob = c(1-p, p))
+# B <- sample(c(FALSE, TRUE), size = N, replace = TRUE, prob = c(1-p, p))
 
 set.seed(191222)
-A <- sample(c(FALSE, TRUE), size = N, replace = TRUE, prob = c(1-p, p))
-B <- sample(c(FALSE, TRUE), size = N, replace = TRUE, prob = c(1-p, p))
+A <- factor(
+  sample(
+    c('Agree', 'Disagree'),
+    size = N,
+    replace = TRUE,
+    prob = c(p, 1-p)
+  )
+)
+B <- factor(
+  sample(
+    c('Agree', 'Disagree'),
+    size = N,
+    replace = TRUE,
+    prob = c(p, 1-p)
+  )
+)
 
-c(mean(A), mean(B))
+c(mean(A == 'Agree'), mean(B == 'Agree'))
 
-(the_test <- fisher.test(A, B))
+(the_test <- fisher.test(A, B, conf.level = .9))
 (tt <- tidy(the_test))
 cbind(A = table(A), B = table(B))
 (the_test <- prop.test(rbind(A = rev(table(A)), B = rev(table(B)))))
